044755543323099bf2aedfb237c500811fdeb5e9
philipnathan <philipnathan168@gmail.com>
2024-11-09 21:23:30 +0800

    config: create new config file

---
+++ config.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+    username = os.getenv("MYSQL_USER")
+    password = os.getenv("MYSQL_PASSWORD")
+    host = os.getenv("MYSQL_HOST")
+    port = os.getenv("MYSQL_PORT")
+    database = os.getenv("MYSQL_DATABASE")
+
+    SQLALCHEMY_DATABASE_URI = (
+        f"mysql+mysqlconnector://{username}:{password}@{host}:{port}/{database}"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class ProductionConfig(Config):
+    pass
+
+
+config = {
+    "development": DevelopmentConfig,
+    "production": ProductionConfig,
+    "default": DevelopmentConfig,
+}
