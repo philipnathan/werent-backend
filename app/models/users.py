01ed9d783aa13f44a0798632f94d8448cf17bef7
philipnathan <philipnathan168@gmail.com>
2024-11-14 14:19:32 +0800

    db: create users entity

---
+++ app/models/users.py
@@ -0,0 +1,42 @@
+import pytz
+import bcrypt
+from datetime import datetime
+
+from ..db import db
+
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    email = db.Column(db.String(80), nullable=False, unique=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    phone_number = db.Column(db.String(14), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False, default=True)
+    created_at = db.Column(
+        db.DateTime(), nullable=False, default=lambda: datetime.now(pytz.UTC)
+    )
+    updated_at = db.Column(
+        db.DateTime(), nullable=True, onupdate=lambda: datetime.now(pytz.UTC)
+    )
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+    def __init__(self, email, username, password):
+        self.email = email
+        self.username = username
+        self.password = self.set_password(password)
+
+    def set_password(self, password):
+        return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt()).decode("utf-8")
+
+    def check_password(self, password):
+        return bcrypt.checkpw(password.encode("utf-8"), self.password.encode("utf-8"))
+
+    def set_to_inactive(self):
+        self.is_active = False
+
+    def to_dict(self):
+        return {"email": self.email, "username": self.username}
