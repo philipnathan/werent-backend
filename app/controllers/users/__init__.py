155b8a881e9ae95a6982a6c421d170974cfb95a7
philipnathan <philipnathan168@gmail.com>
2024-11-17 11:36:10 +0800

    fix: db transaction and Users query

--- app/controllers/users/__init__.py
+++ app/controllers/users/__init__.py
@@ -1,6 +1,4 @@
 from flask import Blueprint, request
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
 from flask_jwt_extended import (
     create_access_token,
     create_refresh_token,
@@ -8,40 +6,42 @@ from flask_jwt_extended import (
     get_jwt_identity,
     get_jwt,
 )
-from sqlalchemy import select
 
 # from connectors.mysql_connectors import connection
-from models.users import Users
+from app.models.users import Users
+from app.db import db
 
-users_routes = Blueprint("users_routes", __name__)
-Session = sessionmaker(connection)
-s = Session()
+users_routes = Blueprint("users_routes", __name__, url_prefix="/api/v1")
 
 
-@users_routes.route("/api/v1/auth/register", methods=["POST"])
+@users_routes.route("/auth/register", methods=["POST"])
 def register_user():
     try:
         NewUser = Users(
             username=request.form["username"],
             email=request.form["email"],
+            password=request.form["password"],
+            phone_number=request.form["phone_number"],
         )
-        NewUser.set_password(request.form["password"])
-        s.add(NewUser)
-        s.commit()
+        # NewUser.set_password(request.form["password"])
+        db.session.add(NewUser)
+        db.session.commit()
     except Exception as e:
         print(e)
-        s.rollback()
+        db.session.rollback()
         return {"message": "Fail to Register New User"}, 500
+
     return {"message": "Success to Create New User"}, 200
 
 
-@users_routes.route("/api/v1/auth/login", methods=["POST"])
+@users_routes.route("/auth/login", methods=["POST"])
 def login_user():
     try:
         email = request.form["email"]
-        user = s.query(Users).filter(Users.email == email).first()
+        # user = s.query(Users).filter(Users.email == email).first()
+        user = Users.query.filter_by(email=email).first()
 
-        if user == None:
+        if user is None:
             return {"message": "User not found"}, 403
 
         if not user.check_password(request.form["password"]):
@@ -51,7 +51,9 @@ def login_user():
             identity=user.id,
             additional_claims={"email": user.email, "id": user.id},
         )
-        s.flush()
+        # s.flush()
+        db.session.flush()
+
         refresh_token = create_refresh_token(identity=user.id)
         return {
             "email": user.email,
@@ -62,11 +64,11 @@ def login_user():
         }, 200
     except Exception as e:
         print(e)
-        s.rollback()
+        db.session.rollback()
         return {"message": "Failed to Login User"}, 500
 
 
-@users_routes.route("/api/v1/auth/refresh", methods=["POST"])
+@users_routes.route("/auth/refresh", methods=["POST"])
 @jwt_required(refresh=True)
 def refresh_token():
     try:
@@ -74,7 +76,8 @@ def refresh_token():
         current_user_id = get_jwt_identity()
 
         # Query the user to confirm they exist
-        user = s.query(Users).filter(Users.id == current_user_id).first()
+        # user = s.query(Users).filter(Users.id == current_user_id).first()
+        user = Users.query.filter_by(id=current_user_id).first()
         if not user:
             return {"message": "User not found"}, 404
 
@@ -93,7 +96,7 @@ def refresh_token():
         return {"message": "Failed to refresh access token"}, 500
 
 
-@users_routes.route("/api/v1/auth/logout", methods=["POST"])
+@users_routes.route("/auth/logout", methods=["POST"])
 @jwt_required()
 def logout_user():
     try:
@@ -104,52 +107,58 @@ def logout_user():
         return {"message": "Failed to logout"}, 500
 
 
-@users_routes.route("/api/v1/users/me", methods=["GET"])
+@users_routes.route("/users/me", methods=["GET"])
 @jwt_required()
 def get_current_user():
     claims = get_jwt()
     return {"claims": claims}
 
 
-@users_routes.route("/api/v1/users/me", methods=["PUT"])
+@users_routes.route("/users/me", methods=["PUT"])
 @jwt_required()
 def update_current_user():
     current_user_id = get_jwt_identity()
     print(current_user_id)
     try:
-        user = s.query(Users).filter(Users.id == current_user_id).first()
+        # user = s.query(Users).filter(Users.id == current_user_id).first()
+        user = Users.query.filter_by(id=current_user_id).first()
         if not user:
             return {"message": "User not found"}, 404
 
         if "password" in request.form:
-            user.set_password(request.form["password"])
-        user.update_at = datetime.now()
+            user.password = user.set_password(request.form["password"])
 
-        s.add(user)
-        s.commit()
+        # tidak perlu update_at, otomatis terisi kalau ada perubahan data di database
+        # user.update_at = datetime.now()
+
+        db.session.add(user)
+        db.session.commit()
         return {"message": "User updated successfully"}, 200
 
     except Exception as e:
         print(e)
-        s.rollback()
+        db.session.rollback()
         return {"message": "Failed to update user"}, 500
 
 
-@users_routes.route("/api/v1/users/me", methods=["DELETE"])
+@users_routes.route("/users/me", methods=["DELETE"])
 @jwt_required()
 def delete_current_user():
     current_user_id = get_jwt_identity()
     try:
-        user = s.query(Users).filter(Users.id == current_user_id).first()
+        # user = s.query(Users).filter(Users.id == current_user_id).first()
+        user = Users.query.filter_by(id=current_user_id).first()
 
         if not user:
             return {"message": "User not found"}, 404
 
-        s.delete(user)
-        s.commit()
+        user.set_to_inactive()
+        db.session.commit()
+        # s.delete(user)
+        # s.commit()
         return {"message": "User deleted successfully"}, 200
 
     except Exception as e:
         print(e)
-        s.rollback()
+        db.session.rollback()
         return {"message": "Failed to delete user"}, 500
